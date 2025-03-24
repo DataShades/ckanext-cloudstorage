cdfc70c8534e94b87b951bc01e2a6c204f1763f2
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2025-03-24 19:37:48 +0200

    chore: bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ckanext-cloudstorage",
-    version="0.3.3",
+    version="0.3.4",
     description="Cloud storage for CKAN",
     classifiers=[],
     keywords="",
