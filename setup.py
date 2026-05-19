5a47d1042b877c90da51f6e95d06ce45671f92a8
Tome Petrovski <tome.petrovski@linkdigital.com.au>
2026-05-19 13:14:30 +0200

    DCEWXCHHG-446/bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="ckanext-cloudstorage",
-    version="0.4.0",
+    version="0.4.2",
     description="Cloud storage for CKAN",
     classifiers=[],
     keywords="",
